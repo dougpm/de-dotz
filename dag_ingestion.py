86fcfaceefade167266acc645743927a2695dad7
dougpm <douglaspmartins0@gmail.com>
2020-10-01 19:59:27 -0300

    test refactor tasks creation

--- dag_ingestion.py
+++ dag_ingestion.py
@@ -5,6 +5,7 @@ Essa DAG le uma lista de arquivos CSV do Storage, criando tabelas para cada um d
 
 import os
 import datetime
+import re
 
 from airflow import configuration
 from airflow import models
@@ -58,45 +59,65 @@ raw_files_bucket = models.Variable.get("raw_files_bucket")
 
 bq_dataset_landing = models.Variable.get("landing_dataset")
 
+def storage_to_bq_task(filename):
+
+    opt_dict = {
+        'file_path': "{}/{}.csv".format(raw_files_bucket, filename),
+        'header': getattr(headers, filename),
+        'destination_table_id': "{}.{}".format(bq_dataset_landing, filename)
+    }
+
+    return DataFlowPythonOperator(
+        task_id='load_{}'.format(filename),
+        py_file=DATAFLOW_PIPELINE_FILE,
+        job_name=re.sub(filename, '_', '-'),
+        options=opt_dict)
+
+
 with models.DAG(dag_id="dotz-ingestao",
                 default_args=DEFAULT_DAG_ARGS,
                 schedule_interval=None) as dag:
 
-    bill_of_materials_opt = {
-        'file_path': "{}/bill_of_materials.csv".format(raw_files_bucket),
-        'header': headers.bill_of_materials,
-        'destination_table_id': "{}.BILL_OF_MATERIALS".format(bq_dataset_landing)   
-    }
+    
+    filenames = 'bill_of_materials comp_boss price_quote'.split()
+    csv_ingestions = []
+    for file in filenames:
+        csv_ingestions.append(storage_to_bq_task(file, headers))
+    # bill_of_materials_opt = {
+    #     'file_path': "{}/bill_of_materials.csv".format(raw_files_bucket),
+    #     'header': headers.bill_of_materials,
+    #     'destination_table_id': "{}.BILL_OF_MATERIALS".format(bq_dataset_landing)   
+    # }
 
-    bill_of_materials = DataFlowPythonOperator(
-        task_id='load_bill_of_materials',
-        py_file=DATAFLOW_PIPELINE_FILE,
-        job_name='bill-of-materials',
-        options=bill_of_materials_opt)
+    # bill_of_materials = DataFlowPythonOperator(
+    #     task_id='load_bill_of_materials',
+    #     py_file=DATAFLOW_PIPELINE_FILE,
+    #     job_name='bill-of-materials',
+    #     options=bill_of_materials_opt)
 
-    comp_boss_opt = {
-        'file_path': "{}/comp_boss.csv".format(raw_files_bucket),
-        'header': headers.comp_boss,
-        'destination_table_id': "{}.COMP_BOSS".format(bq_dataset_landing)   
-    }
+    # comp_boss_opt = {
+    #     'file_path': "{}/comp_boss.csv".format(raw_files_bucket),
+    #     'header': headers.comp_boss,
+    #     'destination_table_id': "{}.COMP_BOSS".format(bq_dataset_landing)   
+    # }
 
-    comp_boss = DataFlowPythonOperator(
-        task_id='load_comp_boss',
-        py_file=DATAFLOW_PIPELINE_FILE,
-        job_name='comp-boss',
-        options=comp_boss_opt)
+    # comp_boss = DataFlowPythonOperator(
+    #     task_id='load_comp_boss',
+    #     py_file=DATAFLOW_PIPELINE_FILE,
+    #     job_name='comp-boss',
+    #     options=comp_boss_opt)
 
-    price_quote_opt = {
-        'file_path': "{}/price_quote.csv".format(raw_files_bucket),
-        'header': headers.price_quote,
-        'destination_table_id': "{}.PRICE_QUOTE".format(bq_dataset_landing)   
-    }
+    # price_quote_opt = {
+    #     'file_path': "{}/price_quote.csv".format(raw_files_bucket),
+    #     'header': headers.price_quote,
+    #     'destination_table_id': "{}.PRICE_QUOTE".format(bq_dataset_landing)   
+    # }
 
-    price_quote = DataFlowPythonOperator(
-        task_id='load_price_quote',
-        py_file=DATAFLOW_PIPELINE_FILE,
-        job_name='price-quote',
-        options=price_quote_opt)
+    # price_quote = DataFlowPythonOperator(
+    #     task_id='load_price_quote',
+    #     py_file=DATAFLOW_PIPELINE_FILE,
+    #     job_name='price-quote',
+    #     options=price_quote_opt)
 
     # bq_task = BigQueryOperator(
     #     task_id="",
