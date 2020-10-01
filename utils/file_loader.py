e96740c90ae386ecfb1f5cba3f92ed9cbcbe398c
dougpm <douglaspmartins0@gmail.com>
2020-10-01 16:59:12 -0300

    swapped regex for ntpath in _get_file_names

--- utils/file_loader.py
+++ utils/file_loader.py
@@ -1,17 +1,27 @@
 import glob
-import re
+import ntpath
 from collections import namedtuple
 
 class FileLoader:
+    def load_files(self, directory: str, file_suffix: str) -> namedtuple:
+
+        """"Reads all files within directory and returns a namedtuple containing files read"""
+        
+        self._directory = directory
+        self._file_suffix = file_suffix
+        self.file_names = self._get_file_names()
+        self._file_tuple = self._create_file_tuple()
+        self.loaded_files = self._file_tuple(*self._load_files_into_tuple())
+        return self.loaded_files
+
     def _get_file_names(self) -> list:
         
         """Reads files with the specified suffix in the specified directory, returns a list with file names"""
         
         file_names = []
-        rx_file_name = r"\/([a-zA-Z0-9_-]*){0}".format(self._file_suffix)
-        for filename in glob.glob(self._directory + "*"):
-            if filename.endswith(self._file_suffix):
-                file_name = re.search(rx_file_name, filename).group(1)
+        for file_path in glob.glob(self._directory + "*"):
+            if file_path.endswith(self._file_suffix):
+                file_name = ntpath.basename(file_path)
                 file_names.append(file_name)
         return file_names
 
@@ -30,16 +40,7 @@ class FileLoader:
         """Reads files in the specified directory, yields files read"""
         
         for file in self.file_names:
-            with open(self._directory + file + self._file_suffix) as f:
+            with open(self._directory + file) as f:
                 yield f.read()
 
-    def load_files(self, directory: str, file_suffix: str) -> namedtuple:
-
-        """"Reads all files within directory and returns a namedtuple containing files read"""
-        
-        self._directory = directory
-        self._file_suffix = file_suffix
-        self.file_names = self._get_file_names()
-        self._file_tuple = self._create_file_tuple()
-        self.loaded_files = self._file_tuple(*self._load_files_into_tuple())
-        return self.loaded_files
+    
